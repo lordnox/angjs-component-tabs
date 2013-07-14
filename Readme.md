7f0af9921dcd4e435c575d8b3583fb00e5d640c8
Tobias <kopelke@gmail.com>
2013-07-14 16:09:41 +0200

    first commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# angjs-component-tabs
+
+  the tabs component from the angularjs homepage
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install lordnox/angjs-component-tabs
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,32 @@
+
+var app       = require('angjs-component-base').module;
+var name      = 'angjs-component-tabs';
+var template  = require('template');
+
+app.directive('tabs', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {},
+    controller: ['$scope', '$element', function($scope, $element) {
+      var panes = $scope.panes = [];
+
+      $scope.select = function(pane) {
+        angular.forEach(panes, function(pane) {
+          pane.selected = false;
+        });
+        pane.selected = true;
+      }
+
+      this.addPane = function(pane) {
+        if (panes.length == 0) $scope.select(pane);
+        panes.push(pane);
+      }
+    },
+    template:template,
+    replace: true
+  }];
+});
+
+module.exports        = name;
+module.exports.module = app;
